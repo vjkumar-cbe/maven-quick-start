4b4aa1d9a853318ba5a7e9e43b41fab816dd91c9
Jayakumar V <jai@test.com>
2021-09-27 19:21:52 +0400

    Adding Unit Tests

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,24 @@
 package clinic.programming.training;
 
+import java.util.List;
+import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+	
+	public int countWords(String words) {
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
+	
+	public void greet() {
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		
+		for (String greet: greetings) {
+			System.out.println("Greeting : " + greet);
+		}
+	}
     
     public Application() {
         System.out.println ("Inside Application");
@@ -10,5 +28,9 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
+		app.greet();
+		
+		int count = app.countWords("I have four words");
+		System.out.println("Word Count : " + count);
     }
 }
